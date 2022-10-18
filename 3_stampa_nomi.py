ef23145562e017caa49b51824276952f305a1c4c
matteolegnani <115645224+matteolegnani@users.noreply.github.com>
2022-10-18 18:18:00 +0200

    Add files via upload

---
+++ 3_stampa_nomi.py
@@ -0,0 +1,20 @@
+Lettera= input("inserisci una lettera: ")
+if (Lettera== "A"):
+    print ("Hai scelto Alina!")
+else: 
+    print ("Hai scelto una lettera diversa da A")
+    if(Lettera == "B"):
+        print("Hai scelto Bruno!")
+    else:
+        print("Hai scelto una lettera diversa da A o B")
+        if(Lettera == "C"):
+            print("HAi scelto Carlo!")
+        else:
+            print("Hai scelto una lettera diversa da A o B o C")
+            if(Lettera == "C"):
+                print("Hai scelto Dario!")
+            else:
+                if(Lettera == "D"):
+                    print("Hai scelto Dario!")
+                else:
+                    print("You choose ... poorly")
