b510b098c5eaed5babb10c2b45de2b4219ab5a79
matteolegnani <115645224+matteolegnani@users.noreply.github.com>
2022-10-18 18:18:54 +0200

    Add files via upload

---
+++ 4_if_numeri.py
@@ -0,0 +1,10 @@
+PrimoNumero = int(input("inserisci il primo numero: "))
+SecondoNumero = int(input("inserisci il secondo numero: ")) 
+if (PrimoNumero>10):
+    print("FUORI SCALA")
+else :
+    print("TUTTO OK")
+if(SecondoNumero>50):
+    print("TROPPO GRANDE")
+else:
+    print("TUTTO OK")
