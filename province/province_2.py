79d13f0a26a2804f327b9e7498afa41014734c44
matteolegnani <115645224+matteolegnani@users.noreply.github.com>
2022-12-31 11:06:51 +0100

    Add files via upload

---
+++ province/province_2.py
@@ -0,0 +1,776 @@
+punteggio = 0
+import random
+m = random.randrange(1,110)
+list=("","AG","AL","AN","AO","AR","AP","AT","AV","BA","BT","BL","BN","BG","BI","BO","BZ","BS","BR","CA","CL","CB","CI","CE","CT","CZ","CH","CO","CS","CR","KR","CN","EN","FM","FE","FI","FG","FC","FR","GE","GO","GR","IM","IS","SP","AQ","LT","LE","LC","LI","LO","LU","MC","MN","MS","MT","ME","MI","MO","MB","NA","NO","NU","OT","OR","PD","PA","PR","PV","PG","PU","PE","PC","PI","PT","PN","PZ","PO","RG","RA","RC","RE","RI","RN","RM","RO","SA","VS","SS","SV","SI","SR","SO","TA","TE","TR","TO","OG","TP","TN","TV","TS","UD","VA","VE","VB","VC","VR","VV","VI","VT")
+print(list[m])
+provincia=input("inserisci la provincia italiana: ")
+
+if(m==1):
+    if(provincia== "agrigento"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==2):
+    if(provincia== "alessandria"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==3):
+    if(provincia== "ancona"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==4):
+    if(provincia== "aosta"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==5):
+    if(provincia== "arezzo"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==6):
+    if(provincia== "ascolipiceno"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==7):
+    if(provincia== "asti"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==8):
+    if(provincia== "avellino"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==9):
+    if(provincia== "bari"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==10):
+    if(provincia== "barletta"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+if(m==11):
+    if(provincia== "belluno"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==12):
+    if(provincia== "benevento"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==13):
+    if(provincia== "bergamo"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==14):
+    if(provincia== "biella"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==15):
+    if(provincia== "bologna"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==16):
+    if(provincia== "bolzano"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==17):
+    if(provincia== "brescia"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==18):
+    if(provincia== "brindisi"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==19):
+    if(provincia== "cagliari"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==20):
+    if(provincia== "caltanissetta"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==21):
+    if(provincia== "campobasso"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==22):
+    if(provincia== "carbonia"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==23):
+    if(provincia== "caserta"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==24):
+    if(provincia== "catania"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==25):
+    if(provincia== "catanzaro"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==26):
+    if(provincia== "chieti"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==27):
+    if(provincia== "como"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==28):
+    if(provincia== "cosenza"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==29):
+    if(provincia== "cremona"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==30):
+    if(provincia== "crotone"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio
+if(m==31):
+    if(provincia== "cuneo"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==32):
+    if(provincia== "enna"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==33):
+    if(provincia== "fermo"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==34):
+    if(provincia== "ferrara"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==35):
+    if(provincia== "firenze"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==36):
+    if(provincia== "foggia"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==37):
+    if(provincia== "forlì"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==38):
+    if(provincia== "frosinone"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==39):
+    if(provincia== "genova"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==40):
+    if(provincia== "gorizia"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==41):
+    if(provincia== "grosseto"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==42):
+    if(provincia== "imperia"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==43):
+    if(provincia== "isernia"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==44):
+    if(provincia== "laspezia"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==45):
+    if(provincia== "aquila"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==46):
+    if(provincia== "latina"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==47):
+    if(provincia== "lecce"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==48):
+    if(provincia== "lecco"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==49):
+    if(provincia== "livorno"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==50):
+    if(provincia== "lodi"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==51):
+    if(provincia== "lucca"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==52):
+    if(provincia== "macerata"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==53):
+    if(provincia== "mantova"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==54):
+    if(provincia== "massacarrara"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==55):
+    if(provincia== "matera"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==56):
+    if(provincia== "messina"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==57):
+    if(provincia== "milano"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+if(m==58):
+    punteggio = punteggio
+    if(provincia== "modena"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==59):
+    if(provincia== "monzabrianza"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==60):
+    if(provincia== "napoli"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==61):
+    if(provincia== "novara"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==62):
+    if(provincia== "nuoro"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==63):
+    if(provincia== "olbia"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==64):
+    if(provincia== "oristano"):
+        punteggio = punteggio +1
+        print("giusto")
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==65):
+    if(provincia== "padova"):
+        punteggio = punteggio +1
+        print("giusto")
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==66):
+    if(provincia== "palermo"):
+        punteggio = punteggio +1
+        print("giusto")
+    else:
+        print("sbagliato")
+if(m==67):
+    if(provincia== "parma"):
+        punteggio = punteggio +1
+        print("giusto")
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==68):
+    if(provincia== "pavia"):
+        punteggio = punteggio +1
+        print("giusto")
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==69):
+    if(provincia== "perugia"):
+        punteggio = punteggio +1
+        print("giusto")
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==70):
+    if(provincia== "pesarourbino"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==71):
+    if(provincia== "pescara"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==72):
+    if(provincia== "piacenza"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==73):
+    if(provincia== "pisa"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==74):
+    if(provincia== "pistoia"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==75):
+    if(provincia== "pordenone"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio
+if(m==76):
+    if(provincia== "potenza"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")   
+        punteggio = punteggio  
+if(m==77):
+    if(provincia== "prato"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+if(m==78):
+    if(provincia== "ragusa"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==79):
+    if(provincia== "ravenna"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==80):
+    if(provincia== "reggiocalabria"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==81):
+    if(provincia== "reggioemilia"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio                               
+if(m==82):
+    if(provincia== "rieti"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==83):
+    if(provincia== "rimini"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio      
+if(m==84):
+    if(provincia== "roma"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==85):
+    if(provincia== "rovigo"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==86):
+    if(provincia== "salerno"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio      
+if(m==87):
+    if(provincia== "mediocampidano"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio 
+if(m==88):
+    if(provincia== "sassari"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==89):
+    if(provincia== "savona"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio  
+if(m==90):
+    if(provincia== "siena"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==91):
+    if(provincia== "siracusa"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")  
+        punteggio = punteggio    
+if(m==92):
+    if(provincia== "sondrio"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==93):
+    if(provincia== "taranto"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio  
+if(m==94):
+    if(provincia== "teramo"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio  
+if(m==95):
+    if(provincia== "terni"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==96):
+    if(provincia== "torino"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato") 
+        punteggio = punteggio 
+if(m==97):
+    if(provincia== "ogliastra"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==98):
+    if(provincia== "trapani"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+if(m==99):
+    if(provincia== "trento"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")  
+        punteggio = punteggio 
+if(m==100):
+    if(provincia== "treviso"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")  
+        punteggio = punteggio 
+if(m==101):
+    if(provincia== "trieste"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")  
+        punteggio = punteggio 
+if(m==102):
+    if(provincia== "udine"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")  
+        punteggio = punteggio  
+if(m==103):
+    if(provincia== "varese"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")  
+        punteggio = punteggio 
+if(m==104):
+    if(provincia== "venezia"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio               
+if(m==105):
+    if(provincia== "verbanocusioossola"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")   
+        punteggio = punteggio 
+if(m==106):
+    if(provincia== "vercelli"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")  
+        punteggio = punteggio   
+if(m==107):
+    if(provincia== "verona"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio  
+if(m==108):
+    if(provincia== "vibovalentia"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio       
+if(m==109):
+    if(provincia== "vicenza"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio            
+if(m==110):
+    if(provincia== "viterbo"):
+        print("giusto")
+        punteggio = punteggio +1
+    else:
+        print("sbagliato")
+        punteggio = punteggio 
+        
+print(punteggio)
