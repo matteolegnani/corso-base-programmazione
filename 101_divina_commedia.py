2a417b1d2a28132329c826d7b52aec6c94089a76
matteolegnani <115645224+matteolegnani@users.noreply.github.com>
2023-01-12 17:09:32 +0100

    Add files via upload

---
+++ 101_divina_commedia.py
@@ -0,0 +1,9 @@
+i=0
+divina="nel mezzo di cammin di nostr avita mi ritrovai in una selva oscura"
+n = len(divina)
+while True:
+    if(i==n):
+        break
+    n=divina[i]
+    print(n)
+    i=i+1
